4f9bd3ca7e839c53632c1daa46c1d0bf7d4c8345
James Chuang <jameschuang13@gmail.com>
2014-01-08 23:01:58 -0800

    change show page for deals and add ability to filter by USA or anywhere

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,6 +1,15 @@
 class DealsController < ApplicationController
 
 	def index
-		@deals = HTTParty.get('http://deals.expedia.com/beta/deals/hotels.json')
+		puts params[:find]
+		@deals = get_deals(params[:find])
+	end
+
+	private
+
+	def get_deals(value)
+		puts value
+		url = 'http://deals.expedia.com/beta/deals/hotels.json?' + value.to_s
+		HTTParty.get(url)
 	end
 end
