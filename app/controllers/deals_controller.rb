5f6d653654a2f814104e07fd1a322d7f149c793c
James Chuang <jameschuang13@gmail.com>
2014-01-09 02:39:33 -0800

    update views for deals

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,14 +1,12 @@
 class DealsController < ApplicationController
 
 	def index
-		puts params[:find]
-		@deals = get_deals(params[:find])
+		@deals = get_deals(params[:options])
 	end
 
 	private
 
 	def get_deals(value)
-		puts value
 		url = 'http://deals.expedia.com/beta/deals/hotels.json?' + value.to_s
 		HTTParty.get(url)
 	end
