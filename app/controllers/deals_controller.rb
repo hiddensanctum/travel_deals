433ab283ad329921ca199ec030ec6dd6c0521147
James Chuang <jameschuang13@gmail.com>
2014-01-09 12:45:40 -0800

    views update with new filter options

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -2,12 +2,18 @@ class DealsController < ApplicationController
 
 	def index
 		@deals = get_deals(params[:options])
+		@date = @deals[0]['checkInDate']
+
+		if params[:options]
+			@deals = @deals.sort_by { |deal| deal['baseRate'] }
+		end
 	end
 
 	private
 
 	def get_deals(value)
-		url = 'http://deals.expedia.com/beta/deals/hotels.json?' + value.to_s
+		url = 'http://deals.expedia.com/beta/deals/hotels.json?' + URI.encode(value.to_s)
+		puts url
 		HTTParty.get(url)
 	end
 end
