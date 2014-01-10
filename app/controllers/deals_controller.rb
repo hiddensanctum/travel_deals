824ae3eeb578b28cbeb289aaa94fdfe4189d1952
James Chuang <jameschuang13@gmail.com>
2014-01-09 22:40:11 -0800

    remvoe unneccesary code

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -9,32 +9,6 @@ class DealsController < ApplicationController
 		end
 	end
 
-	def get_rating_img(deal)
-		if deal['guestRating'] == 5
-			image_tag("urating5_0.png")
-		elsif deal['guestRating'] >= 4.5
-			image_tag("urating4_5.png")
-		elsif deal['guestRating'] >= 4.0
-			image_tag("urating4_0.png")
-		elsif deal['guestRating'] >= 3.5
-		 image_tag("urating3_5.png")
-		elsif deal['guestRating'] >= 3.0
-			image_tag("urating3_0.png")
-		elsif deal['guestRating'] >= 2.5
-			image_tag("urating2_5.png")
-		elsif deal['guestRating'] >= 2.0
-			image_tag("urating2_0.png")
-		elsif deal['guestRating'] >= 1.5
-			image_tag("urating1_5.png")
-		elsif deal['guestRating'] >= 1.0
-			image_tag("urating1_0.png")
-		elsif deal['guestRating'] >= 0.5
-			image_tag("urating0_5.png")
-		else
-			image_tag("urating0_0.png")
-		end
-	end
-
 	private
 
 	def get_deals(value)
